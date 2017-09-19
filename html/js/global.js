de063be4c64574d8b6056f0db7a09ec9cf3b809d
jiangdengqiang <498593805@qq.com>
2017-09-19 18:01:08 +0800

    new beta first page

--- html/js/global.js
+++ html/js/global.js
@@ -1,2 +1,24 @@
-﻿$(function(){
+﻿;$(function($){
+    var method = {
+        selecterUI:function(){
+            $(".selectpicker").each(function(){
+                $(this).selectpicker();
+            })
+        },
+        accordion:function(){
+            $(".module-accordion").each(function(){
+                $(this).find(".module-list").on("click",function(){
+                    if($(this).hasClass("open")){
+                        $(this).removeClass("open");
+                        $(this).find(".module-list-two").stop().slideUp(250)
+                    }else {
+                        $(".module-accordion").find(".module-list-two").slideUp(250);
+                        $(this).addClass("open").siblings(".module-list").removeClass("open") && $(this).find(".module-list-two").stop().slideDown(400);
+                    }
+                })
+            })
+        }
+    }
+    method.selecterUI();
+    method.accordion();
 })
